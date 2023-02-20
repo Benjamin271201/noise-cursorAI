92f8c0d8e18598cda75f8bdf6c8ac964fccfb11b
thisismyname113 <125874934+thisismyname113@users.noreply.github.com>
2023-02-20 20:34:09 +0700

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,15 +6,15 @@ twitter.com/russetPotato
 */
 
 const SPIN_MULTIPLIER = 45;
-const MIN_PARTICLE_COUNT = 2500;
-const MAX_PARTICLE_COUNT = 2600;
+const MIN_PARTICLE_COUNT = 1500;
+const MAX_PARTICLE_COUNT = 1700;
 const MIN_PARTICLE_SIZE = 2;
 const MAX_PARTICLE_SIZE = 15;
 const MIN_FORCE = 0.75;
 const MAX_FORCE = 0.95;
 const REPULSION_RADIUS = 125;
 const REPULSION_STRENGTH = 0.325;
-const IMG_RESIZED_WIDTH = 1450;
+const IMG_RESIZED_WIDTH = 1200;
 const IMG_SCAN_STEPS = 2;
 const NUM_OF_MOUSES = 1;
 
@@ -31,7 +31,7 @@ var particles = [];
 var indices = [];
 var imgIndex = 0;
 var drawType = 0;
-var particleCount = 2500;
+var particleCount = 1500;
 var maxSize = 0;
 var img;
 
