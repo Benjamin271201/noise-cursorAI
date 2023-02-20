994cf5321b6ebc3e66754485ff8b2f84ac584093
thisismyname113 <125874934+thisismyname113@users.noreply.github.com>
2023-02-20 21:16:34 +0700

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -22,6 +22,7 @@ const DrawTypes = {
 	Rect: 0,
 	Ellipse: 1,
 	Triangle: 2,
+	Plus: 3,
 };
 
 const mouses = [];
@@ -86,6 +87,24 @@ function draw() {
 					0, particle.size, 
 					particle.size * 0.5, particle.size * -0.5);
 				break;
+			case DrawTypes.Plus://Rect will now make plus signs
+				rect(0, 0, particle.size*0.25, particle.size);
+				rect(0, 0, particle.size, particle.size*0.25);
+				break;
+				
+/////////////////this next section is taken from https://openprocessing.org/sketch/1695925 and then modified to avoid freezing.
+let p = [];
+  f = frameCount;
+for (y = 100; y < 200; y += 20) {//I lowered the number of loops to prevent extreme lag
+    beginShape();
+    for (x = 0; x < 36; x += 6) {//I lowered the number of loops here as well. the result of this is a cool particle field effect around the arrows
+      dy = (80 / (1 + pow(x - 150, 4) / 8e6)) * noise(x / 30 + f / 50 + y);
+      p.push({ x, y: y-dy });
+      vertex(x, y-dy);
+    }
+    endShape();
+  }/////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+				
 		}
 		
 		pop();
