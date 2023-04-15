55aa4a7292311e24b9699382281772f7adc39cc6
thisismyname113 <125874934+thisismyname113@users.noreply.github.com>
2023-04-15 21:20:11 +0700

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -15,7 +15,7 @@ const MAX_FORCE = 0.95;
 const REPULSION_RADIUS = 125;
 const REPULSION_STRENGTH = 0.325;
 const IMG_RESIZED_WIDTH = 750;
-const IMG_SCAN_STEPS = 8;
+const IMG_SCAN_STEPS = 4;
 const NUM_OF_MOUSES = 1;
 
 const DrawTypes = {
